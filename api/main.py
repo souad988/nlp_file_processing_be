4fe3f21babb55e1c90cd4abc26eafaad00c0d451
Souad El Mansouri <souadelmansouri88@gmail.com>
2024-06-01 11:54:44 +0100

    Configure db and add file upload model and andpoint

--- api/main.py
+++ api/main.py
@@ -1,15 +1,16 @@
 import shutil
-from fastapi import FastAPI, File, UploadFile
+import time
+from fastapi import Depends, FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from pathlib import Path
+from sqlalchemy.orm import Session
+from app.db_config import SessionLocal, engine
+from app import models
 
 
 app = FastAPI()
-
-
-
 
 # Define allowed origins
 origins = [
@@ -19,21 +20,39 @@ origins = [
     # Add other origins as needed
 ]
 
-# Add CORS middleware
+# Add the CORS middleware to the application.
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins,  # Allows requests from the specified origins
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Allows all methods (POST, GET, etc.)
+    allow_headers=["*"],  # Allows all headers
 )
+
+
+
+
+models.Base.metadata.create_all(bind=engine)
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+
+
 
 @app.get("/")
 def root():
     return {"message": "hello from be!"}
 
 @app.post("/uploadfile")
-async def upload_file(file: UploadFile = File(...)):
+async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db)):
     try:
         # Define the upload directory
         upload_dir = Path("uploads")
@@ -45,9 +64,13 @@ async def upload_file(file: UploadFile = File(...)):
         # Save the uploaded file to the defined location
         with open(file_location, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
-        
+        time.sleep(3)
+
+        file_record = models.FileUpload(file_name=file.filename)
+        file_record.save(db)
+        print('file record',file_record,file)
         # Return a success message with file details
-        return JSONResponse(content={"filename": file.filename, "message": "File uploaded successfully"})
+        return JSONResponse(content={"file": {"filename":file.filename, "id": file_record.id}, "message": "File uploaded successfully"})
     except Exception as e:
         # Handle any exceptions that occur during file upload
         return JSONResponse(content={"error": str(e)}, status_code=500)
