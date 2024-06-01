5d645a7eed4108a0d0768010fa9087158f983f05
Souad El Mansouri <souadelmansouri88@gmail.com>
2024-06-02 00:14:10 +0100

    Add question answer integrations

--- api/main.py
+++ api/main.py
@@ -8,9 +8,11 @@ from pathlib import Path
 from sqlalchemy.orm import Session
 from app.db_config import SessionLocal, engine
 from app import models
+from app.file_processing.pdf_processing import PDFProcessor
 
 
 app = FastAPI()
+processor = PDFProcessor() 
 
 # Define allowed origins
 origins = [
@@ -64,13 +66,60 @@ async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db
         # Save the uploaded file to the defined location
         with open(file_location, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
-        time.sleep(3)
+        time.sleep(1)
 
         file_record = models.FileUpload(file_name=file.filename)
         file_record.save(db)
-        print('file record',file_record,file)
+        
+        # Load documents
+        docs = processor.load_documents([str(file_location)])
+        
+        # Split documents
+        splits = processor.split_documents(docs)
+        
+        # Create vector store
+        processor.create_vector_store(document_id = file.filename, splits = splits)  #
+        
+        
         # Return a success message with file details
         return JSONResponse(content={"file": {"filename":file.filename, "id": file_record.id}, "message": "File uploaded successfully"})
+    except Exception as e:
+        # Handle any exceptions that occur during file upload
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+    
+
+
+# Define request body model
+class QuestionAnswerRequest(BaseModel):
+    question: str
+    filename: str
+
+# Define response model
+class QuestionAnswerResponse(BaseModel):
+    answer: str
+
+
+# Define endpoint for question answering
+@app.post("/question-answer", response_model=QuestionAnswerResponse)
+async def question_answer(qa_request: QuestionAnswerRequest):
+    try:
+        # Load documents
+        docs = processor.load_documents(["uploads/"+qa_request.filename])
+        
+        # Split documents
+        splits = processor.split_documents(docs)
+        
+        # Create vector store
+        #vectordb = processor.create_vector_store(splits)
+        
+        vectordb = processor.get_vector_store(document_id= qa_request.filename)
+        # Initialize QA chain
+        qa_chain = processor.initialize_qa_chain(vectordb)
+        
+        # Answer question
+        answer = processor.answer_question(qa_chain, qa_request.question)
+        
+        return JSONResponse(content={"answer": answer, "message": "question answered successfully"})
     except Exception as e:
         # Handle any exceptions that occur during file upload
         return JSONResponse(content={"error": str(e)}, status_code=500)
